143119da0bb0599df1c9e2649581f0710c81980b
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-02-22 17:45:00 +0900

    P182 list4-05

---
+++ chap4/arrow.js
@@ -0,0 +1,8 @@
+let getTriangle = (base, height) => {
+  return base * height / 2;
+}
+
+console.log('三角形の面積:' + getTriangle(5, 2));
+
+// 更に簡略化
+// let getTriangle = (base, height) => base * height / 2;
