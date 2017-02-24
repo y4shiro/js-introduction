bb666a957827986fb626652fecf4976d3912d95a
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-02-24 17:47:57 +0900

    Edited chain.js

--- chap5/chain.js
+++ chap5/chain.js
@@ -17,3 +17,28 @@ Dog.prototype.bark = function() {
 var d = new Dog();
 d.walk();
 d.bark();
+
+var Cat = function() {
+  Animal.call(this);
+};
+Cat.prototype = new Animal();
+Cat.prototype.bark = function() {
+  console.log('ニャーン');
+}
+
+var c = new Cat();
+c.walk();
+c.bark();
+
+var Serval = function() {
+  Cat.call(this);
+};
+Serval.prototype = new Cat();
+Serval.prototype.talk = function() {
+  console.log('サーバルだよ!');
+}
+
+var s = new Serval();
+s.walk();
+s.bark();
+s.talk();
