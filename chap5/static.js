29800c8a6227f4125ccd13623bc16e8d925945d2
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-02-24 17:25:03 +0900

    P242 list5-18

---
+++ chap5/static.js
@@ -0,0 +1,19 @@
+var Area = function() { }; // コンストラクター
+
+Area.version = '1.0'; // 静的プロパティの定義
+
+// 静的メソッドtriangleの定義
+Area.triangle = function(base, height) {
+  return base * height / 2;
+};
+
+// 静的メソッドdiamondの定義
+Area.diamond = function(width, height) {
+  return width * height / 2;
+};
+
+console.log('Areaクラスのバージョン:' + Area.version);
+console.log('三角形の面積:' + Area.triangle(5, 3));
+
+var a = new Area();
+console.log('ひし形の面積:' + a.diamond(10, 2));
