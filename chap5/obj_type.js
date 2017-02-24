a1efaa25b63f953d800f63b71f76763f2e234ec8
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-02-24 18:45:11 +0900

    P249 list5-22

--- chap5/obj_type.js
+++ chap5/obj_type.js
@@ -7,3 +7,6 @@ var h = new Hamster();
 console.log(a.constructor === Animal);
 console.log(h.constructor === Animal);
 console.log(h.constructor === Hamster);
+
+console.log(h instanceof Animal);
+console.log(h instanceof Hamster);
