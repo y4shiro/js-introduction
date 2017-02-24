f0d0e1c437e7806fcbc9204ccf913ccc5073547c
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-02-24 17:04:58 +0900

    P240 list5-17

---
+++ chap5/literal2.js
@@ -0,0 +1,13 @@
+var Member = function (firstName, lastName){
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+Member.prototype = {
+  getName : function() {
+    return this.lastName + ' ' + this.firstName;
+  },
+  toString : function() {
+    return this.lastName + this.firstName;
+  }
+};
