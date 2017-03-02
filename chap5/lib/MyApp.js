891f81b0f64cf2bd7fdb6b4d2be04699d4bc4d75
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-02 12:25:40 +0900

    P269 list5-44 list5-45

---
+++ chap5/lib/MyApp.js
@@ -0,0 +1,18 @@
+const SECRET_VALUE = Symbol();
+
+export default class {
+  constructor(secret) {
+    this.hoge = 'hoge';
+    this.foo = 'foo';
+
+    this[SECRET_VALUE] = secret;
+  }
+
+  checkValue(secret) {
+    return this[SECRET_VALUE] === secret;
+  }
+
+  getValue() {
+    return this[SECRET_VALUE];
+  }
+}
