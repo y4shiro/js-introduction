7e6938ea477e53af17f9b7c1023d9050d97bbef8
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-06 16:35:57 +0900

    P318 list6-26

---
+++ chap6/scripts/file_reader.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("file").addEventListener('change', function(e) {
+    var input = document.getElementById("file").files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function() {
+      document.getElementById("result").textContent = reader.result;
+    }, true);
+    reader.readAsText(input, 'UTF-8');
+  }, true);
+});
