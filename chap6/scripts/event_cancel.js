d2d756995a8c8e6880fd8cfcc7935abd1c879386
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-07 18:22:42 +0900

    P350 list6-46

---
+++ chap6/scripts/event_cancel.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('inner').addEventListener('click', function(e) {
+    window.alert('#innerリスナーが発生しました');
+    e.stopPropagation();
+  }, false);
+
+  document.getElementById('inner').addEventListener('click', function(e) {
+    window.alert('#innerリスナー2が発生しました');
+  }, false);
+
+
+  document.getElementById('outer').addEventListener('click', function(e) {
+    window.alert('#outerリスナーが発生しました');
+  }, false);
+}, false);
