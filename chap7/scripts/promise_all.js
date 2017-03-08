c3715ed246eedb9b530d23474dbda18ccc724943
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-08 18:19:28 +0900

    P417 list7-34

---
+++ chap7/scripts/promise_all.js
@@ -0,0 +1,24 @@
+function asyncProcess(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (value) {
+        resolve(`入力値: ${value}`);
+      } else {
+        reject('入力は空です');
+      }
+    }, 500);
+  });
+}
+
+Promise.all([
+  asyncProcess('1'),
+  asyncProcess('2'),
+  asyncProcess('3')
+]).then(
+  response => {
+    console.log(response);
+  },
+  error => {
+    console.log(`エラー: ${error}`);
+  }
+);
